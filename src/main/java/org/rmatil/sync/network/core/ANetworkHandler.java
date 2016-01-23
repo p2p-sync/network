555142492c4f9f9307b129edc38cbeaaf1c14e78
Raphael Matile <raphael.matile@gmail.com>
2016-01-23 16:57:14 +0100

    Remove futureDirect Listener

--- src/main/java/org/rmatil/sync/network/core/ANetworkHandler.java
+++ src/main/java/org/rmatil/sync/network/core/ANetworkHandler.java
@@ -5,6 +5,7 @@ import net.tomp2p.futures.FutureDirect;
 import org.rmatil.sync.network.api.*;
 import org.rmatil.sync.network.core.exception.ConnectionFailedException;
 import org.rmatil.sync.network.core.exception.ObjectSendFailedException;
+import org.rmatil.sync.network.core.messaging.FutureDirectListener;
 import org.rmatil.sync.network.core.model.ClientDevice;
 import org.rmatil.sync.network.core.model.ClientLocation;
 import org.slf4j.Logger;
@@ -77,63 +78,38 @@ public abstract class ANetworkHandler<T> implements INetworkHandler<T>, IRespons
         List<ClientLocation> clientLocations = request.getReceiverAddresses();
 
         // offer file
-        final CountDownLatch latch = new CountDownLatch(clientLocations.size());
         for (ClientLocation entry : clientLocations) {
             if (entry.getPeerAddress().equals(this.client.getPeerAddress())) {
                 logger.debug("Ignoring receiver address " + entry.getIpAddress() + ":" + entry.getPort() + " since it is the own client's address");
-                latch.countDown();
                 continue;
             }
 
             logger.debug("Sending request " + request.getExchangeId() + " to client " + entry.getIpAddress() + ":" + entry.getPort() + ". Timestamp: " + System.currentTimeMillis());
             try {
-                final FutureDirect futureDirect = this.client.sendDirect(entry.getPeerAddress(), request);
+                FutureDirect futureDirect = this.client.sendDirect(entry.getPeerAddress(), request);
+                FutureDirectListener futureDirectListener = new FutureDirectListener();
+                futureDirect.addListener(futureDirectListener);
 
-                final ClientDevice clientDevice = new ClientDevice(
-                        client.getUser().getUserName(),
+                // await using a listener instead of the future direct directly
+                futureDirectListener.await();
+
+                if (futureDirect.isFailed()) {
+                    throw new ObjectSendFailedException("Failed to sent request " + request.getExchangeId() + ". Message: " + futureDirect.failedReason());
+                }
+
+                ClientDevice clientDevice = new ClientDevice(
+                        this.client.getUser().getUserName(),
                         entry.getClientDeviceId(),
                         entry.getPeerAddress()
                 );
 
-
-                // if the future is already completed, the future listener will not be called,
-                // therefore we have to countdown already here!
-                if (futureDirect.isCompleted()) {
-                    if (futureDirect.isFailed()) {
-                        logger.error("Failed to sent request " + request.getExchangeId() + ". Message: " + futureDirect.failedReason());
-                    }
-
-                    notifiedClients.put(clientDevice, futureDirect);
-                    latch.countDown();
-                } else {
-                    // operation complete is only invoked, if the future is not completed yet!
-                    futureDirect.addListener(new BaseFutureAdapter<FutureDirect>() {
-                        @Override
-                        public void operationComplete(FutureDirect future)
-                                throws Exception {
-                            logger.trace("Got operationComplete");
-                            // we got a response here
-                            latch.countDown();
-
-                            if (futureDirect.isFailed()) {
-                                logger.error("Failed to sent request " + request.getExchangeId() + ". Message: " + futureDirect.failedReason());
-                            }
-
-                            notifiedClients.put(clientDevice, futureDirect);
-                        }
-                    });
-                }
+                this.notifiedClients.put(clientDevice, futureDirect);
 
             } catch (ObjectSendFailedException e) {
                 logger.error("Failed to send request to client " + entry.getClientDeviceId() + " (" + entry.getPeerAddress().inetAddress().getHostAddress() + ":" + entry.getPeerAddress().tcpPort() + "). Message: " + e.getMessage());
+            } catch (InterruptedException e) {
+                logger.error("Failed to send request to client " + entry.getClientDeviceId() + " (" + entry.getPeerAddress().inetAddress().getHostAddress() + ":" + entry.getPeerAddress().tcpPort() + "). Got interrupted while waiting for completion. Message: " + e.getMessage());
             }
-        }
-
-        // await using a listener instead of the future direct directly
-        try {
-            latch.await();
-        } catch (InterruptedException e) {
-            logger.error("Got interrupted while waiting for completion of sending request to all clients. Message: " + e.getMessage(), e);
         }
 
         // init count down latch with size of all clients
